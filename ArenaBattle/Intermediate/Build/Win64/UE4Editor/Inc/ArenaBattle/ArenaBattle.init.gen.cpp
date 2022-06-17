afb5d430c627460f570c6a37c26141846f0c3125
Jong Hyun Choi <choich323@naver.com>
2022-06-17 18:15:15 +0900

    Chapter7 Design of Animation System

--- ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
+++ ArenaBattle/Intermediate/Build/Win64/UE4Editor/Inc/ArenaBattle/ArenaBattle.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeArenaBattle_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xC227207C,
-				0x9BD66F76,
+				0xEA43079C,
+				0x1EB55EC1,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
--- ArenaBattle/Source/ArenaBattle/ABCharacter.cpp
+++ ArenaBattle/Source/ArenaBattle/ABCharacter.cpp
@@ -34,6 +34,7 @@ AABCharacter::AABCharacter()
 
 	ArmLengthSpeed = 3.0f;
 	ArmRotationSpeed = 10.0f;
+	GetCharacterMovement()->JumpZVelocity = 800.0f;
 }
 
 // Called when the game starts or when spawned
@@ -110,6 +111,7 @@ void AABCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	PlayerInputComponent->BindAction(TEXT("ViewChange"), EInputEvent::IE_Pressed, this, &AABCharacter::ViewChange);
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AABCharacter::Jump);
 
 	PlayerInputComponent->BindAxis(TEXT("Updown"), this, &AABCharacter::UpDown);
 	PlayerInputComponent->BindAxis(TEXT("LeftRight"), this, &AABCharacter::LeftRight);
